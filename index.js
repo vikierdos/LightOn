29c4457dac9b1dafb822b4ee6bc62ba0b62c20a6
vikierdos <csicsorka@gmail.com>
2024-04-04 11:40:09 +0200

    randomizalo_fv

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+var lista = randomizalo();
+
 function szinAllitas(ELEM){
     ELEM.toggleClass("sarga")
 }
 
 function lamparaKattint(lista){
     
+}
+
+function randomizalo(){
+    var lampakAllapota= [false, false, false,false, false, false,false, false, false];
+    for (let index = 0; index < 8; index++) {
+        lampakAllapota[index] = (Math.random()*101-1) <= 20;
+    }
+    return lampakAllapota;
 }
